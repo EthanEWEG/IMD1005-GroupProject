423e6cd3517045ea440bffed2e5e1933d0018e4f
raniajabi <rania.jabi26@gmail.com>
2025-03-30 19:24:55 -0400

    editing chatbox

--- scripts/zhuzhu.js
+++ scripts/zhuzhu.js
@@ -1,14 +1,6 @@
 
 /*Carousel */
-window.addEventListener('DOMContentLoaded', () => {
-    const carousel = document.querySelector('.carousel');
-    
-    const images = Array.from(carousel.children);
-    images.forEach(img => {
-        const clone = img.cloneNode(true);
-        carousel.appendChild(clone);
-    });
-});
+
 
 
 /*Chat box */
